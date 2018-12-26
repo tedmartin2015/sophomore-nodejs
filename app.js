56f472322cec8c4a03b33aff5010fb8189925bdc
Ted Martin Hermosisima <tedmartin2015@users.noreply.github.com>
2018-12-26 10:32:04 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const people = require('./people.json');
+
+const app = express();
+const port = 5009;
+
+app.engine('pug', require('pug').__express);
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render(
+        'index', 
+        { 
+            title: "Homepage",
+            people: people.profiles
+        });
+});
+
+app.get('/profile', (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render(
+        'profile', 
+        {
+            title: `About ${person.firstname} ${person.lastname}`,
+            person,
+        });
+});
+
+const server = app.listen(port, () => {
+    console.log(`Express running -> PORT ${port}`);
+});
